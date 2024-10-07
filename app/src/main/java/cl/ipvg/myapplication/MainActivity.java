47bed0f283aa37f66228a44a06998dac4240a71f
Alumnos <felipe.jara9@virginiogomez.cl>
2024-10-07 13:57:46 -0300

    Initial commit

---
+++ app/src/main/java/cl/ipvg/myapplication/MainActivity.java
@@ -0,0 +1,59 @@
+package cl.ipvg.myapplication;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
+
+
+    TextView textView;
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        textView = findViewById(R.id.textView);
+
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+
+
+                        if (location != null) {
+                            // Logic to handle location object
+                            textView.setText(location.toString());
+                            location.toString();
+                        }
+                    }
+                });
+
+        //Intent i = new Intent(this, MapsActivity.class);
+        //startActivity(i);
+    }
+}
