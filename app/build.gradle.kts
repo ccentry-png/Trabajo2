f8628d1753c7f86425a7a2039459a9fbcb1e715b
carlos <carloscontador1@virginiogomez.cl>
2024-10-07 22:27:36 -0300

    fix crash on startup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "cl.ipvg.myapplication"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "cl.ipvg.myapplication"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -35,16 +35,15 @@ android {
     }
 }
 
-dependencies {
 
-        implementation("androidx.core:core-splashscreen:1.2.0-alpha02")
+
+dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.play.services.maps)
-    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/cl/ipvg/myapplication/MainActivity.java
+++ app/src/main/java/cl/ipvg/myapplication/MainActivity.java
@@ -1,12 +1,9 @@
 package cl.ipvg.myapplication;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,67 +11,62 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
+
 import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MainActivity extends AppCompatActivity {
-
-    private FusedLocationProviderClient fusedLocationClient;
-
-    Intent i = new Intent(this, MapsActivity.class);
-
-    TextView textView;
-    Button button;
-
-    @SuppressLint("MissingPermission")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        setTheme(R.style.SplashScreen);
-
-        //button = findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(i);
-            }
-        });
 
 
 
+    public class MainActivity extends AppCompatActivity {
 
 
-        //textView = findViewById(R.id.textView);
+        private final int splashDuracion =5000;
+
+
+        //private FusedLocationProviderClient fusedLocationClient;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+            setContentView(R.layout.activity_main);
 
 
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
+            Intent intent = new Intent(this, MapsActivity.class);
+            new Handler().postDelayed(new Runnable() {
+                                          @Override
+                                          public void run() {
+                                              startActivity(intent);
+                                              finish();
+                                          }
+                                      }
+                    , splashDuracion);
 
 
-                        if (location != null) {
-                            // Logic to handle location object
-                            textView.setText(location.toString());
-
-                        }
-                    }
-                });
+           /* fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
 
 
+                            if (location != null) {
+                                // Logic to handle location object
+                                textView.setText(location.toString());
+
+                            }
+                        }*/
+
+
+
+
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        }
     }
-}
+
